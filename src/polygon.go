64f7ce37a9f67b3f8cc7b5544b5a361a036cdfbc
kalyan vadlamani <kalyan.vadlamani@us.ibm.com>
2019-09-27 10:40:16 -0400

    ext coordinates

--- src/polygon.go
+++ src/polygon.go
@@ -5,6 +5,7 @@ import (
 	"github.com/paulmach/orb/geojson"
 	"github.com/skelterjohn/geom"
 	"github.com/tidwall/gjson"
+	"github.com/uber/h3-go"
 	_ "log"
 )
 
@@ -22,29 +23,6 @@ func (p Polygon) InteriorRings() []geom.Polygon {
 	return p.Interior
 }
 
-func (p Polygon) ContainsCoord(c geom.Coord) bool {
-
-	ext := p.ExteriorRing()
-
-	contains := false
-
-	if ext.ContainsCoord(c) {
-
-		contains = true
-
-		for _, int := range p.InteriorRings() {
-
-			if int.ContainsCoord(c) {
-				contains = false
-				break
-			}
-		}
-	}
-
-	return contains
-}
-
-
 func GjsonCoordsToPolygon(r gjson.Result) (Polygon, error) {
 
 	rings := r.Array()
@@ -52,7 +30,7 @@ func GjsonCoordsToPolygon(r gjson.Result) (Polygon, error) {
 	count_rings := len(rings)
 	count_interior := count_rings - 1
 
-	exterior, err := gjsonLinearringtogeompolygon(rings[0])
+	exterior, err := gjsonLinearRingTogeomPolygon(rings[0])
 
 	panic(err)
 
@@ -60,7 +38,7 @@ func GjsonCoordsToPolygon(r gjson.Result) (Polygon, error) {
 
 	for i := 1; i <= count_interior; i++ {
 
-		poly, err := gjsonLinearringtogeompolygon(rings[i])
+		poly, err := gjsonLinearRingTogeomPolygon(rings[i])
 
 		panic(err)
 
@@ -85,6 +63,16 @@ func NewCoordinateFromLatLons(lat float64, lon float64) (geom.Coord, error) {
 	return *coord, nil
 }
 
+func H3CoordFromLatLons(lat float64, lon float64) (h3.GeoCoord, error) {
+
+	coord := new(h3.GeoCoord)
+
+	coord.Latitude = lat
+	coord.Longitude = lon
+
+	return *coord, nil
+}
+
 func NewPolygonFromCoords(coords []geom.Coord) (geom.Polygon, error) {
 
 	path := geom.Path{}
@@ -99,7 +87,7 @@ func NewPolygonFromCoords(coords []geom.Coord) (geom.Polygon, error) {
 	return *poly, nil
 }
 
-func gjsonLinearringtogeompolygon(r gjson.Result) (geom.Polygon, error) {
+func gjsonLinearRingTogeomPolygon(r gjson.Result) (geom.Polygon, error) {
 
 	coords := make([]geom.Coord, 0)
 
