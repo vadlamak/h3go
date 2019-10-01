69372e4424cda1e2ff45b725345b2534304f5055
kalyan vadlamani <kalyan.vadlamani@us.ibm.com>
2019-10-01 16:18:57 -0400

    updates

--- src/ingest-polygon.go
+++ src/ingest-polygon.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/tidwall/gjson"
 	"github.com/uber/h3-go"
+	"io"
 	"log"
 	"os"
 	"time"
@@ -38,13 +39,6 @@ func getGeoJson(record []string) string {
 	return record[6]
 }
 
-func getGeocoord(arr []float64) h3.GeoCoord {
-	return h3.GeoCoord{
-		Latitude:  arr[1],
-		Longitude: arr[0],
-	}
-}
-
 func main() {
 	start := time.Now()
 	fileName := getFileName()
@@ -55,28 +49,18 @@ func main() {
 
 	r := getFileReader(fileName)
 
-	//skip header
+	//read line # 0 - header
 	header, _ := r.Read()
-	r.Read() //skip multiPolygon
 	fmt.Println(header)
-	rec, _ := r.Read()
-	jsonStr := getGeoJson(rec)
-	polygonType:= gjson.Get(jsonStr,"type")
-	println("type:",polygonType.String())
-	if(polygonType.String()=="Polygon") {
-		result := gjson.Get(jsonStr, "coordinates")
-		//gjson.Get(jsonStr, "name.last")
-		polygon, _ := GjsonCoordsToPolygon(result)
-		extCoord := polygon.Exterior.Vertices()
-		extRing:= polygon.ExteriorRing().Vertices()
-		interiorRings := polygon.InteriorRings()
-		println("exterior Coordinates")
-		for _, coord := range extCoord {
-			print("x: %f", coord.X)
-			println(", y: %f", coord.Y)
+	for i := 0; i < 5; i++ {
+		// Read each record from csv
+		record, err := r.Read()
+		if err == io.EOF {
+			logger.Println("finished reading the file")
+			break
 		}
 
-		println(extCoord)
+		handlePolygons(record)
 	}
 
 	t := time.Now()
@@ -85,4 +69,30 @@ func main() {
 	logger.Println("total records written: %d", count)
 
 	fmt.Println(&buf)
+}
+
+// query KV store of choice
+func getAssetsForH3Index(h3Indices []h3.H3Index) {
+	c := getConn()
+	for _, h3Index := range h3Indices {
+		assets, _ := c.AsyncDo("SMEMBERS", h3Index)
+	}
+}
+
+func handlePolygons(record []string) {
+	jsonStr := getGeoJson(record)
+	geoJsonType := gjson.Get(jsonStr, "type").String()
+	switch geoJsonType {
+	case "Polygon":
+		coordinates := gjson.Get(jsonStr, "coordinates")
+
+		h3polygon := CoordinatesToH3Polygon(coordinates)
+		h3Indices := h3.Polyfill(h3polygon, 9)
+		println("len:", len(h3Indices))
+		getAssetsForH3Index(h3Indices)
+	case "MultiPolygon":
+	default:
+		panic("unsupported type:" + geoJsonType)
+
+	}
 }
--- src/polygon.go
+++ src/polygon.go
@@ -1,35 +1,18 @@
 package main
 
 import (
-	"github.com/paulmach/orb/geojson"
-	"github.com/skelterjohn/geom"
 	"github.com/tidwall/gjson"
-	h3 "github.com/uber/h3-go"
+	"github.com/uber/h3-go"
 	_ "log"
 )
 
-type Polygon struct {
-	geojson.Polygon `json:",omitempty"`
-	Exterior        geom.Polygon   `json:"exterior"`
-	Interior        []geom.Polygon `json:"interior"`
-}
-
-func (p Polygon) ExteriorRing() geom.Polygon {
-	return p.Exterior
-}
-
-func (p Polygon) InteriorRings() []geom.Polygon {
-	return p.Interior
-}
-
-func GjsonToH3Polygon(r gjson.Result)(h3.GeoPolygon){
-
+func CoordinatesToH3Polygon(r gjson.Result) h3.GeoPolygon {
 	rings := r.Array()
-
 	count_rings := len(rings)
 	count_interior := count_rings - 1
+	println("count interior:", count_interior)
 
-	exterior, err := gjsonLinearRingToH3Geocoord(rings[0])
+	exterior, err := linearRingToH3Geocoord(rings[0])
 
 	if err != nil {
 		println(err)
@@ -39,7 +22,7 @@ func GjsonToH3Polygon(r gjson.Result)(h3.GeoPolygon){
 
 	for i := 1; i <= count_interior; i++ {
 
-		poly, err := gjsonLinearRingToH3Geocoord(rings[i])
+		poly, err := linearRingToH3Geocoord(rings[i])
 
 		if err != nil {
 			println(err)
@@ -50,40 +33,14 @@ func GjsonToH3Polygon(r gjson.Result)(h3.GeoPolygon){
 
 	polygon := h3.GeoPolygon{
 		Geofence: exterior,
-		Holes: interior,
+		Holes:    interior,
 	}
 
 	return polygon
 
 }
 
-/*
-type GeoCoord struct {
-	Latitude, Longitude float64
-}
-
-func (g GeoCoord) toCPtr() *C.GeoCoord {
-	return &C.GeoCoord{
-		lat: C.double(deg2rad * g.Latitude),
-		lon: C.double(deg2rad * g.Longitude),
-	}
-}
-
-func (g GeoCoord) toC() C.GeoCoord {
-	return *g.toCPtr()
-}
-
-// GeoPolygon is a geofence with 0 or more geofence holes
-type GeoPolygon struct {
-	// Geofence is the exterior boundary of the polygon
-	Geofence []GeoCoord
-
-	// Holes is a slice of interior boundary (holes) in the polygon
-	Holes [][]GeoCoord
-}
-**/
-
-func gjsonLinearRingToH3Geocoord(r gjson.Result) ([]h3.GeoCoord, error) {
+func linearRingToH3Geocoord(r gjson.Result) ([]h3.GeoCoord, error) {
 
 	coords := make([]h3.GeoCoord, 0)
 
@@ -93,62 +50,13 @@ func gjsonLinearRingToH3Geocoord(r gjson.Result) ([]h3.GeoCoord, error) {
 
 		lat := lonlat[1].Float()
 		lon := lonlat[0].Float()
-		//println("lat:", lat)
-		//println("lon:", lon)
+		//fmt.Print("\nlat:lon: ",lat,lon)
 
 		coord, _ := H3CoordFromLatLons(lat, lon)
 		coords = append(coords, coord)
 	}
 
-	return coords,nil
-}
-
-func GjsonCoordsToPolygon(r gjson.Result) (Polygon, error) {
-	//println("result",r.String())
-	//println("isArray", r.IsArray())
-	//println("r.Type", r.Type)
-	rings := r.Array()
-
-	count_rings := len(rings)
-	count_interior := count_rings - 1
-
-	//println("rings:", rings)
-	//println("ring[0]:",rings[0])
-	exterior, err := gjsonLinearRingTogeomPolygon(rings[0])
-
-	if err != nil {
-		println(err)
-	}
-
-	interior := make([]geom.Polygon, count_interior)
-
-	for i := 1; i <= count_interior; i++ {
-
-		poly, err := gjsonLinearRingTogeomPolygon(rings[i])
-
-		if err != nil {
-			println(err)
-		}
-
-		interior = append(interior, poly)
-	}
-
-	polygon := Polygon{
-		Exterior: exterior,
-		Interior: interior,
-	}
-
-	return polygon, nil
-}
-
-func NewCoordinateFromLatLons(lat float64, lon float64) (geom.Coord, error) {
-
-	coord := new(geom.Coord)
-
-	coord.Y = lat
-	coord.X = lon
-
-	return *coord, nil
+	return coords, nil
 }
 
 func H3CoordFromLatLons(lat float64, lon float64) (h3.GeoCoord, error) {
@@ -159,39 +67,4 @@ func H3CoordFromLatLons(lat float64, lon float64) (h3.GeoCoord, error) {
 	coord.Longitude = lon
 
 	return *coord, nil
-}
-
-func NewPolygonFromCoords(coords []geom.Coord) (geom.Polygon, error) {
-
-	path := geom.Path{}
-
-	for _, c := range coords {
-		path.AddVertex(c)
-	}
-
-	poly := new(geom.Polygon)
-	//println("new polygon:", poly)
-	poly.Path = path
-
-	return *poly, nil
-}
-
-func gjsonLinearRingTogeomPolygon(r gjson.Result) (geom.Polygon, error) {
-
-	coords := make([]geom.Coord, 0)
-
-	for _, pt := range r.Array() {
-
-		lonlat := pt.Array()
-
-		lat := lonlat[1].Float()
-		lon := lonlat[0].Float()
-		//println("lat:", lat)
-		//println("lon:", lon)
-
-		coord, _ := NewCoordinateFromLatLons(lat, lon)
-		coords = append(coords, coord)
-	}
-
-	return NewPolygonFromCoords(coords)
 }
